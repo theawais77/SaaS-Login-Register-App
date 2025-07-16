15b9e32d97a5ffdbc647707edb00584db09db356
Awais Tahir <theowais456@gmail.com>
2025-07-16 21:12:22 +0500

    Changed url in passport.js

--- configs/passportConfig.js
+++ configs/passportConfig.js
@@ -5,7 +5,7 @@ const User = require('../models/user');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/auth/google/callback',
+  callbackURL: 'https://saa-s-login-register-app.vercel.app/auth/google/callback',
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     const existingUser = await User.findOne({ email: profile.emails[0].value });
